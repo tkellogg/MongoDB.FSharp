49f0ce4e69518d62d787b0274e69d14fdf3c725a
Sebastian Faltoni <sebastian.faltoni@gmail.com>
2015-07-06 15:18:10 +0200

    modified package script to use new dll names

--- package.ps1
+++ package.ps1
@@ -1,4 +1,4 @@
 mkdir dist -Force | Out-Null
 mkdir build\lib\net45\ -Force | Out-Null
-cp .\MongoDB.FSharp\bin\Release\MongoDB.FSharp.* .\build\lib\net45\
+cp .\MongoDB.FSharp\bin\Release\MongoDB.Driver.FSharp.* .\build\lib\net45\
 .\.nuget\NuGet.exe pack -OutputDirectory .\dist\ -BasePath .\build\
